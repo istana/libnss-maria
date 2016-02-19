9c246447f0c3b90fa31bed71b09d319403eb507c
Ivan Stana <stiipa@centrum.sk>
2016-02-19 04:01:04 -0800

    add libconfig, attempt to add ctest

--- src/configuration.h
+++ src/configuration.h
@@ -1,3 +1,28 @@
-#include <jansson.h>
+#define NSSMARIACONFIG "/etc/libnss-maria.conf"
+#include <libconfig.h>
 
-json_t load_config()
+config_t *nssconfig;
+config_init(nssconfig);
+
+if(config_read_file(nssconfig, NSSMARIACONFIG)) {
+
+
+
+} else {
+  puts("error found in file %s, message: %s on line: %i", config_error_file(nssconfig), config_error_text(nssconfig), config_error_line(nsconfig));
+}
+
+config_setting_t *database_config, *nss_queries;
+
+database_config = config_lookup(nssconfig, "database_config");
+nss_queries = config_lookup(nssconfig, "nssqueries");
+
+if(database_config == NULL) { 
+  puts ()
+}
+
+
+char *dbname, ;
+config_lookup_string(nssconfig, "database", &dbname);
+
+config_destroy(nssconfig);
