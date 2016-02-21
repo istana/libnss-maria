25d27ec3774726de73cae7676f603c72012bd197
tex <tex@christiemonteiro.lan>
2016-02-21 16:48:57 +0100

    config loader finished

--- src/configuration.h
+++ src/configuration.h
@@ -1,12 +1,13 @@
 #define NSSMARIACONFIG "/etc/libnss-maria.conf"
 #include <libconfig.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 
 typedef struct Nssmaria_configs {
   char dbhost[1024];
-  unsigned int dbport;
+  char dbport[1024];
   char dbuser[1024];
   char dbpass[1024];
   char dbname[1024];
@@ -47,12 +48,20 @@ void nss_maria_initialize_config(Nssmaria_config *config) {
   strncpy(config->dbuser, "root", 1023);
   strncpy(config->dbpass, "", 1023);
   strncpy(config->dbname, "libnss_maria", 1023);
-  config->dbport = 3309;
+  strncpy(config->dbport, "3309", 1023);
 
   config->getpwnam[0] = config->getpwuid[0] = config->getpwent[0] = '\0';
   config->getspnam[0] = config->getspent[0] = '\0';
   config->getgrnam[0] = config->getgrid[0] = config->getgrent[0] = '\0';
   config->memsbygid[0] = config->gidsbymem[0] = '\0';
+}
+
+void nss_maria_load_setting(config_t conffile, char *dest, const char *path) {
+  const char *buffer = malloc(1024 * sizeof(char));
+
+  if(config_lookup_string(&conffile, path, &buffer) == CONFIG_TRUE) {
+    strncpy(dest, buffer, 1023);
+  };
 }
 
 int nss_maria_populate_config_from_file(char *filepath, Nssmaria_config *config) {
@@ -66,6 +75,23 @@ int nss_maria_populate_config_from_file(char *filepath, Nssmaria_config *config)
     if(config_read(&config_nssmaria, config_stream) == CONFIG_TRUE) {
       printf("ok");
 
+      nss_maria_load_setting(config_nssmaria, config->dbhost, "database_settings.host");
+      nss_maria_load_setting(config_nssmaria, config->dbname, "database_settings.database");
+      nss_maria_load_setting(config_nssmaria, config->dbuser, "database_settings.username");
+      nss_maria_load_setting(config_nssmaria, config->dbpass, "database_settings.password");
+      nss_maria_load_setting(config_nssmaria, config->dbport, "database_settings.port");
+
+      nss_maria_load_setting(config_nssmaria, config->getpwnam, "nss_queries.getpwnam");
+      nss_maria_load_setting(config_nssmaria, config->getpwuid, "nss_queries.getpwuid");
+      nss_maria_load_setting(config_nssmaria, config->getspnam, "nss_queries.getspnam");
+      nss_maria_load_setting(config_nssmaria, config->getspent, "nss_queries.getspent");
+      nss_maria_load_setting(config_nssmaria, config->getgrnam, "nss_queries.getgrnam");
+      nss_maria_load_setting(config_nssmaria, config->getgrid, "nss_queries.getgrid");
+      nss_maria_load_setting(config_nssmaria, config->getgrent, "nss_queries.getgrent");
+      nss_maria_load_setting(config_nssmaria, config->memsbygid, "nss_queries.memsbygid");
+      nss_maria_load_setting(config_nssmaria, config->gidsbymem, "nss_queries.gidsbymem");
+
+      config_destroy (&config_nssmaria);
       return 0;
     } else {
       printf("error found in file %s, message: %s on line: %i",
@@ -73,10 +99,10 @@ int nss_maria_populate_config_from_file(char *filepath, Nssmaria_config *config)
         config_error_text(&config_nssmaria),
         config_error_line(&config_nssmaria)
       );
+      config_destroy(&config_nssmaria);
       return 1;
     }
-
-    config_destroy (&config_nssmaria);
+    
   } else {
     printf("Opening file : Failed\n");
     printf ("Error no is : %d\n", errno);
@@ -85,24 +111,3 @@ int nss_maria_populate_config_from_file(char *filepath, Nssmaria_config *config)
 
   fclose(config_stream);
 }
-
-/*
-
-
-
-
-config_setting_t *database_config, *nss_queries;
-
-database_config = config_lookup(nssconfig, "database_config");
-nss_queries = config_lookup(nssconfig, "nssqueries");
-
-if(database_config == NULL) { 
-  puts ()
-}
-
-
-char *dbname, ;
-config_lookup_string(nssconfig, "database", &dbname);
-
-config_destroy(nssconfig);
-*/
--- test/libnss_maria_config_test.c
+++ test/libnss_maria_config_test.c
@@ -9,7 +9,7 @@ Test(maria_config, initialize_config) {
   cr_assert_str_eq(config.dbuser, "root");
   cr_assert_str_eq(config.dbpass, "");
   cr_assert_str_eq(config.dbname, "libnss_maria");
-  cr_assert_eq(config.dbport, 3309);
+  cr_assert_str_eq(config.dbport, "3309");
 
   cr_assert_str_eq(config.getpwnam, "");
   cr_assert_str_eq(config.getpwuid, "");
@@ -37,4 +37,16 @@ Test(maria_config, populate_config_from_file) {
   Nssmaria_config config;
   nss_maria_initialize_config(&config);
   cr_assert_eq(nss_maria_populate_config_from_file("./test/fixtures/libnss-maria.conf", &config), 0);
+
+  printf("%s", config.dbhost);
+  cr_assert_str_eq(config.dbhost, "myrtana.sk");
+  cr_assert_str_eq(config.dbname, "nss_maria");
+  cr_assert_str_eq(config.dbuser, "unss_maria");
+  cr_assert_str_eq(config.dbpass, "Isabelle");
+  cr_assert_str_eq(config.dbport, "9000");
+
+  cr_assert_str_eq(config.getpwnam, "SELECT users.username AS username, 'x' AS password, users.id AS uid, users.gid AS gid, \
+users.gecos AS gecos, users.homedir AS homedir, users.shell AS shell FROM users INNER JOIN groups_users ON users.id = groups_users.user_id \
+INNER JOIN groups ON groups_users.group_id = groups.id WHERE users.active = 1 AND users.gid = groups.id AND users.username = ? \
+ORDER BY groups.id LIMIT 1");
 }
