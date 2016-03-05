77e51672c8ec4eca45d73d3b86ffeb838a3a69db
tex <tex@christiemonteiro.lan>
2016-03-05 01:52:07 +0100

    prepared statements wip

--- src/libnss-maria.c
+++ src/libnss-maria.c
@@ -23,8 +23,53 @@ enum nss_status _nss_maria_getpwnam_r(const char *name, struct passwd *pwd, char
     puts("could not connect to the database");
     printf("server information: %s", mysql_get_server_info(mysql_handle));
     printf("error number: %u, error: %s\n", mysql_errno(mysql_handle), mysql_error(mysql_handle));
+    return 10;
   }
 
+
+
+  MYSQL_STMT *statement = mysql_stmt_init(connection);
+
+  if(statement == NULL) {
+    puts("could not connect to the database");
+    return 10;
+  }
+
+  if(mysql_stmt_prepare(statement, settings.getpwnam, strlen(settings.getpwnam)) != 0) {
+    puts("cannot prepare statement");
+    return 10;
+  }
+
+  MYSQL_BIND bind[1];
+
+  bind[0].buffer_type = MYSQL_TYPE_STRING;
+  bind[0].buffer = "testuserXXX";
+  bind[0].buffer_length = strlen("testuserXXX");
+
+  bind[0].is_null = (my_bool*)0;
+  bind[0].length = 0;
+  bind[0].is_unsigned = 0;
+
+  if(mysql_stmt_bind_param(statement, bind) != 0) {
+    puts("cannot bind params");
+    return 10;
+  }
+
+  if (mysql_stmt_execute(statement))
+  {
+    fprintf(stderr, " mysql_stmt_execute(), 1 failed\n");
+    fprintf(stderr, " %s\n", mysql_stmt_error(statement));
+    exit(0);
+  }
+
+  /* Get the number of affected rows */
+  uint affected_rows = mysql_stmt_affected_rows(statement);
+  fprintf(stdout, " total affected rows(insert 1): %lu\n",
+                  (unsigned long) affected_rows);
+
+
+
+  mysql_stmt_close(statement);
   mysql_close(mysql_handle);
 
   return 0;
