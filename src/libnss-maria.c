49947cad9ad45d8fc23abcd277a6d78d4dfe7e01
tex <tex@christiemonteiro.lan>
2016-03-05 01:05:21 +0100

    print server info when cannot connect to the database

--- src/libnss-maria.c
+++ src/libnss-maria.c
@@ -21,7 +21,8 @@ enum nss_status _nss_maria_getpwnam_r(const char *name, struct passwd *pwd, char
 
   if(connection == NULL) {
     puts("could not connect to the database");
-    printf("error number: %u, error: %s", mysql_errno(mysql_handle), mysql_error(mysql_handle));
+    printf("server information: %s", mysql_get_server_info(mysql_handle));
+    printf("error number: %u, error: %s\n", mysql_errno(mysql_handle), mysql_error(mysql_handle));
   }
 
   mysql_close(mysql_handle);
