98d601641be3bb17f786c8c0215d479ef892876f
Tex <ronon@myrtana.sk>
2016-11-11 09:37:02 -0800

    make the file lowercased

--- src/controllers/nssmaria_libconfig.h
+++ src/controllers/nssmaria_libconfig.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-#include "models/Nssmaria_configurations.h"
+#include "models/nssmaria_configurations.h"
 #include "helpers/nssmaria_libconfig.h"
 
 Nssmaria_configuration nss_maria_read_config_from_file(char *path);
--- src/helpers/nssmaria_libconfig.h
+++ src/helpers/nssmaria_libconfig.h
@@ -1,7 +1,7 @@
 #include <libconfig.h>
 #include <stdlib.h>
 #include <string.h>
-#include "models/Nssmaria_configuration.h"
+#include "models/nssmaria_configuration.h"
 
 void nss_maria_initialize_config(Nssmaria_configuration *config);
 void nss_maria_load_setting(config_t libconfig_object, char *destination, const char *selector);
