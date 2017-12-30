a40dfc2cde3736cc1a8f2db9547f5561dfdfa819
Ivan Stana <^_^@myrtana.sk>
2017-12-30 18:25:00 +0100

    add sleep, because Docker cannot handle speed

--- build-docker.sh
+++ build-docker.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 rm -rf Debug
 mkdir Debug
+sleep 2
 docker exec -it libnssmaria_build_1 sh -c 'cd /home/libnss-maria/Debug && cmake -D CMAKE_BUILD_TYPE=Debug .. && make && ctest --verbose'
