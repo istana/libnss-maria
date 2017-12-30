73d3d0a3eb0e2708a0627f683cc38ea8f6633cb8
Ivan Stana <^_^@myrtana.sk>
2017-12-30 18:25:11 +0100

    pthread skeleton

--- src/nss/passwd.c
+++ src/nss/passwd.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <pthread.h>
 #include "passwd.h"
 #include "../logger/maria_logger.h"
+
+pthread_key_t last_uid;
+//thread_local long long int last_uid = -1;
 
 // shouldn't use malloc at all, but put everything from passwd into *buffer variable
 enum nss_status _nss_maria_getpwnam_r (
@@ -73,6 +77,11 @@ enum nss_status _nss_maria_getpwent_r (
   size_t buflen,
   int *errnop
 ) {
+  int value = 9000;
+
+  pthread_key_create(&last_uid, NULL);
+  pthread_setspecific(last_uid, &value);
+
   FILE *logfile;
   logfile = fopen ("/var/log/maria.log", "a+");
   fprintf(
@@ -102,6 +111,11 @@ enum nss_status _nss_maria_getpwent_r (
     buflen    
   );
   maria_log(msg);
+
+  int thread_value = *((long int *) pthread_getspecific(last_uid));
+  char msg2[1024];
+  sprintf(msg2, "last_uid: %i", thread_value);
+  maria_log(msg2);
 
   if (result_buf->pw_uid > 0) {
     return NSS_STATUS_NOTFOUND;
