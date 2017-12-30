527ab473f1cdbeb760d74dee7664b55785d4045a
Ivan Stana <^_^@myrtana.sk>
2017-12-30 18:25:05 +0100

    interface for pwd works with system, getpwent cannot work unless there is a shared state

--- src/nss/passwd.c
+++ src/nss/passwd.c
@@ -1,7 +1,10 @@
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include "passwd.h"
+#include "../logger/maria_logger.h"
 
+// shouldn't use malloc at all, but put everything from passwd into *buffer variable
 enum nss_status _nss_maria_getpwnam_r (
   const char *name_query,
   struct passwd *result_buf,
@@ -64,26 +67,60 @@ enum nss_status _nss_maria_getpwuid_r (
   return NSS_STATUS_SUCCESS;
 }
 
-enum nss_status _nss_mysql_getpwent_r (
+enum nss_status _nss_maria_getpwent_r (
   struct passwd *result_buf,
   char *buffer,
   size_t buflen,
   int *errnop
 ) {
+  FILE *logfile;
+  logfile = fopen ("/var/log/maria.log", "a+");
+  fprintf(
+    logfile,
+    "_nss_maria_getpwent_r name:%s password:%s gecos:%s dir:%s shell:%s buffer:%s buflen:%lu",
+    result_buf->pw_name,
+    result_buf->pw_passwd,
+    result_buf->pw_gecos,
+    result_buf->pw_dir,
+    result_buf->pw_shell,
+    buffer,
+    buflen    
+  );
+
+  fclose(logfile);
+
+  char msg[1024];
+  sprintf(
+    msg,
+    "_nss_maria_getpwent_r name:%s password:%s gecos:%s dir:%s shell:%s buffer:%s buflen:%lu",
+    result_buf->pw_name,
+    result_buf->pw_passwd,
+    result_buf->pw_gecos,
+    result_buf->pw_dir,
+    result_buf->pw_shell,
+    buffer,
+    buflen    
+  );
+  maria_log(msg);
+
+  if (result_buf->pw_uid > 0) {
+    return NSS_STATUS_NOTFOUND;
+  }
+
   char *name = malloc(sizeof(char) * 256);
   char *password = malloc(sizeof(char) * 256);
   char *gecos = malloc(sizeof(char) * 256);
   char *dir = malloc(sizeof(char) * 256);
   char *shell = malloc(sizeof(char) * 256);
 
-  strncpy(name, "testuser", 255);
-  strncpy(password, "x", 255);
+  strncpy(buffer, "testuser", 255);
+  strncpy(buffer+9, "x", 255);
   strncpy(gecos, "Unprivileged User UID,", 255);
   strncpy(dir, "/var/empty,", 255);
   strncpy(shell, "/usr/bin/false", 255);
 
-  result_buf->pw_name = name;
-  result_buf->pw_passwd = password;
+  result_buf->pw_name = buffer;
+  result_buf->pw_passwd = buffer+9;
   result_buf->pw_uid = 12345;
   result_buf->pw_gid = 6789;
   result_buf->pw_gecos = gecos;
@@ -93,3 +130,16 @@ enum nss_status _nss_mysql_getpwent_r (
   return NSS_STATUS_SUCCESS;
 }
 
+// closes the database
+// but database queries don't use persistent connections
+// so empty for now
+// is used from getpwent?
+enum nss_status _nss_maria_endpwent (void) {
+  return NSS_STATUS_SUCCESS;
+}
+
+// rewinds the database, how???
+// is used from getpwent
+enum nss_status _nss_maria_setpwent (void) {
+  return NSS_STATUS_SUCCESS;
+}
