298d3464d7ac99f265518c80b6ecfe810e0e80b6
Ivan Stana <^_^@myrtana.sk>
2017-12-30 18:24:29 +0100

    set up first integration test

--- test/integration/nss_module.sh
+++ test/integration/nss_module.sh
@@ -2,6 +2,17 @@
 
 docker-compose down
 docker-compose build && docker-compose up -d
+
+docker exec -it libnssmaria_sys_1 getent passwd root
+docker exec -it libnssmaria_sys_1 getent passwd testuser
+RETURNCODE=$?
+
 docker-compose down
 
-#docker -it sys_1 getent passwd testuser
+if [ "$RETURNCODE" -gt 0 ]; then
+  echo "TEST FAIL"
+else
+  echo "TEST SUCCESS"
+fi
+
+exit $RETURNCODE;
