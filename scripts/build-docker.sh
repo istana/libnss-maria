b7117d27be1e5ecc62f88b48e5ba6336b2bbb14e
Ivan Stana <^_^@myrtana.sk>
2018-01-08 22:24:58 +0100

    move build scripts to a separate directory

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+cd ..
 rm -rf Debug
 mkdir Debug
 sleep 2
 docker exec -it libnssmaria_build_1 sh -c 'cd /home/libnss-maria/Debug && cmake -D CMAKE_BUILD_TYPE=Debug .. && make && ctest --verbose'
+cd scripts
