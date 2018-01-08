358519aaabd4022b51f1cf8043fe55e77d9b2ff5
Ivan Stana <^_^@myrtana.sk>
2018-01-08 22:25:16 +0100

    decrease sleep in docker build

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -2,6 +2,6 @@
 cd ..
 rm -rf Debug
 mkdir Debug
-sleep 2
+sleep 1
 docker exec -it libnssmaria_build_1 sh -c 'cd /home/libnss-maria/Debug && cmake -D CMAKE_BUILD_TYPE=Debug .. && make && ctest --verbose'
 cd scripts
