c2f439496035793dd3e459b73ee48aca4a602928
Ivan Stana <^_^@myrtana.sk>
2019-09-07 12:12:23 +0200

    vagrant: stop build process when previous command fails

--- scripts/build-vagrant.sh
+++ scripts/build-vagrant.sh
@@ -10,24 +10,24 @@ SCRIPTS_DIR=`dirname $REAL_PATH`
 echo SCRIPTS $SCRIPTS_DIR
 DEBUG_BUILD=${SCRIPTS_DIR}/../Debug
 
-rm -rf $DEBUG_BUILD
-mkdir $DEBUG_BUILD
+rm -rf $DEBUG_BUILD &&
+mkdir $DEBUG_BUILD &&
 sleep 1
 
 dostuff 'cd /home/libnss-maria/Debug && cmake -D CMAKE_BUILD_TYPE=Debug .. \
-&& make && ctest --verbose'
+&& make && ctest --verbose' &&
 
 # go to scripts directory, ugly, but well..
-cd $SCRIPTS_DIR
+cd $SCRIPTS_DIR &&
 
-dostuff 'sudo rm /lib/libnss_maria.so*'
-dostuff 'sudo cp /home/libnss-maria/Debug/src/libnss_maria.so.2.0.0 /lib/libnss_maria.so.2.0.0'
-dostuff 'sudo ln -s /lib/libnss_maria.so.2.0.0 /lib/libnss_maria.so'
-dostuff 'sudo ln -s /lib/libnss_maria.so.2.0.0 /lib/libnss_maria.so.2'
-dostuff 'sudo chmod 644 /lib/libnss_maria.so.2.0.0'
+dostuff 'sudo rm /lib/libnss_maria.so*' &&
+dostuff 'sudo cp /home/libnss-maria/Debug/src/libnss_maria.so.2.0.0 /lib/libnss_maria.so.2.0.0' &&
+dostuff 'sudo ln -s /lib/libnss_maria.so.2.0.0 /lib/libnss_maria.so' &&
+dostuff 'sudo ln -s /lib/libnss_maria.so.2.0.0 /lib/libnss_maria.so.2' &&
+dostuff 'sudo chmod 644 /lib/libnss_maria.so.2.0.0' &&
 
-dostuff 'sudo cp /home/libnss-maria/examples/sos-sso/nsswitch.conf /etc'
-dostuff 'sudo cp /home/libnss-maria/examples/sos-sso/libnss-maria.conf /etc'
-dostuff 'sudo cp /home/libnss-maria/examples/sos-sso/libnss-maria-root.conf /etc'
+dostuff 'sudo cp /home/libnss-maria/examples/sos-sso/nsswitch.conf /etc' &&
+dostuff 'sudo cp /home/libnss-maria/examples/sos-sso/libnss-maria.conf /etc' &&
+dostuff 'sudo cp /home/libnss-maria/examples/sos-sso/libnss-maria-root.conf /etc' &&
 
 dostuff 'getent passwd katarina || echo "No user found"'
