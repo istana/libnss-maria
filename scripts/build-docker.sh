a25512fc90934dff9dcda65b55c0904f19eee31f
Ivan Stana <^_^@myrtana.sk>
2020-07-04 20:15:41 +0200

    Allow to set source config file via env variable

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker-compose exec sys sh -c "/home/libnss-maria/scripts/compile_and_test.sh"
+docker-compose exec sys sh -c "CONFIG_FILE=\"libnss-maria-docker.conf\" /home/libnss-maria/scripts/compile_and_test.sh"
--- scripts/compile_and_test.sh
+++ scripts/compile_and_test.sh
@@ -10,6 +10,9 @@ else
 fi
 
 SUDO_COMMAND=$([[ -z $TEST_USE_SUDO ]] && echo "" || echo "sudo")
+CONFIG_FILE=${CONFIG_FILE:-libnss-maria.conf}
+
+echo "CONFIG_FILE: $CONFIG_FILE"
 
 rm -rf "$HOME_PATH/$TARGET"
 mkdir "$HOME_PATH/$TARGET"
@@ -31,12 +34,8 @@ if [[ $? -eq 0 && -z $COMPILE_ONLY ]]; then
   $SUDO_COMMAND chmod 644 /lib/libnss_maria.so.2.0.0 &&
 
   $SUDO_COMMAND cp -bf /home/libnss-maria/examples/sos-sso/nsswitch.conf /etc &&
-  $SUDO_COMMAND cp -bf /home/libnss-maria/examples/sos-sso/libnss-maria.conf /etc &&
+  $SUDO_COMMAND cp -bf /home/libnss-maria/examples/sos-sso/$CONFIG_FILE /etc/libnss-maria.conf &&
   $SUDO_COMMAND cp -bf /home/libnss-maria/examples/sos-sso/libnss-maria-root.conf /etc
-
-  if ! [[ -z $DOCKER ]]; then
-    $SUDO_COMMAND cp /home/libnss-maria/examples/sos-sso/libnss-maria-docker.conf /etc/libnss-maria.conf
-  fi
 
   ${HOME_PATH}/test/integration/nss_module.bats
 fi
