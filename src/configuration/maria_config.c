ec2edd287f743259829e907267c58deb82800b3e
Ivan Stana <^_^@myrtana.sk>
2021-09-18 23:40:26 +0200

    Return correct return value when opening a file fails

--- src/configuration/maria_config.c
+++ src/configuration/maria_config.c
@@ -1,14 +1,11 @@
 #include "maria_config.h"
 
 int maria_read_config(Maria_config* config, const char *path, const char *root_path, int is_root) {
-  int root_config_ok = 0;
-
   maria_initialize_config(config);
-  int config_ok = maria_set_config_from_file(path, config);
 
-  if(is_root) {
-    root_config_ok = maria_set_root_config_from_file(root_path, config);
+  if(!is_root) {
+    return maria_set_config_from_file(path, config);
   }
 
-  return config_ok && root_config_ok;
+  return maria_set_root_config_from_file(root_path, config);
 }
