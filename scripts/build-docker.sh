bb1a4bd43cd4b4ab35056ece343c4633e8195d5f
Ivan Stana <^_^@myrtana.sk>
2021-09-18 16:17:19 +0200

    Fix "the input device is not a TTY"

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,1 +1,1 @@
-docker-compose exec sys sh -c "CONFIG_FILE=\"libnss-maria-docker.conf\" VERBOSE=${VERBOSE} /home/libnss-maria/scripts/compile_and_test.sh"
+docker-compose exec $( (( CI == 1 )) && printf %s '-T' ) sys sh -c "CONFIG_FILE=\"libnss-maria-docker.conf\" VERBOSE=${VERBOSE} /home/libnss-maria/scripts/compile_and_test.sh"
