87a519818999b8ec57971652456030d847213246
Ivan Stana <^_^@myrtana.sk>
2021-09-18 18:11:31 +0200

    Allow more options in build-docker

--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,1 +1,9 @@
-docker-compose exec $( (( CI == 1 )) && printf %s '-T' ) sys sh -c "CONFIG_FILE=\"libnss-maria-docker.conf\" VERBOSE=${VERBOSE} /home/libnss-maria/scripts/compile_and_test.sh"
+docker-compose exec $( (( CI == 1 )) && printf %s '-T' ) sys sh -c "\
+CLIENT_FILE=${CLIENT_FILE} \
+COMPILE_ONLY=${COMPILE_ONLY} \
+CONFIG_FILE=${CONFIG_FILE:=libnss-maria-docker.conf} \
+EXAMPLE_SET=${EXAMPLE_SET} \
+RELEASE=${RELEASE} \
+VERBOSE=${VERBOSE} \
+TEST_USE_SSL=${TEST_USE_SSL} \
+/home/libnss-maria/scripts/compile_and_test.sh"
